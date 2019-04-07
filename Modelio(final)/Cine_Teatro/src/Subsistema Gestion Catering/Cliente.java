47d001ee61bbf50f0ef96b305b02d81a899dfc2f
luispozas <luispozas@users.noreply.github.com>
2019-04-07 14:05:32 +0200

    n
    n

---
+++ Modelio(final)/Cine_Teatro/src/Subsistema Gestion Catering/Cliente.java
@@ -0,0 +1,15 @@
+package Subsistema Gestion Catering;
+
+import java.util.ArrayList;
+import java.util.List;
+import com.modeliosoft.modelio.javadesigner.annotations.objid;
+
+@objid ("5c4e7c57-d77f-48dc-b8d4-2d3b113476de")
+public class Cliente extends Persona {
+    @objid ("dc20d944-9ded-45f1-8e5b-084350bd799c")
+    private boolean Comensal;
+
+    @objid ("d242b7a4-dd93-438a-943b-87d2e3324b4d")
+    public List<TransferPedido>  = new ArrayList<TransferPedido> ();
+
+}
